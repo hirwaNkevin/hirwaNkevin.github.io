62aa89b6b8db4cbcf2a5175bb6ad24eb7d7c1d18
hirwaNkevin <hirwankevin@gmail.com>
2023-10-14 19:43:19 +0200

    add navbar toggler functonality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-console.log("Started..");
+var toggleBtn = document.querySelector('#menu_toggler>span');
+var closeBtn = document.querySelector('#close');
+var navLink = document.querySelectorAll('.nav-link');
+toggleBtn.addEventListener('click' ,  show);
+
+closeBtn.addEventListener('click' ,  remove);
+navLink.forEach(item=>item.addEventListener('click' ,  remove));
+
+function show(){
+	var sideBar = document.querySelector('.nav-list');
+	sideBar.classList.toggle('active');
+}
+
+function remove(){
+	var sideBar = document.querySelector('.nav-list');
+	sideBar.classList.remove('active');
+}
